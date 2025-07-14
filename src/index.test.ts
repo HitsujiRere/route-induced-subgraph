50f7f23b7a559a3e21587522466161ddf143c30f
HitsujiRere <55770125+HitsujiRere@users.noreply.github.com>
2025-07-14 09:55:23 +0900

    Init

---
+++ src/index.test.ts
@@ -0,0 +1,8 @@
+import { describe, expect, it } from "vitest";
+import { sum } from "./index";
+
+describe("sum", () => {
+	it("should sum two numbers", () => {
+		expect(sum(1, 2)).toBe(3);
+	});
+});
